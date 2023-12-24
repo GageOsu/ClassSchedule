028134d150b26702a6949c3a7d6c702cec54087a
Daniil_Morozov <dannilmorozov@gmail.com>
2023-12-25 01:04:55 +0300

    Создать Окна CreateDayWindow UpdateDayWindow

---
+++ CurriculumSchedule/CurriculumSchedule/View/ViewDayCRUDOperation/CreateDayWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CurriculumSchedule.View.ViewDayCRUDOperation
+{
+    /// <summary>
+    /// Логика взаимодействия для CreateDayWindow.xaml
+    /// </summary>
+    public partial class CreateDayWindow : Window
+    {
+        public CreateDayWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
