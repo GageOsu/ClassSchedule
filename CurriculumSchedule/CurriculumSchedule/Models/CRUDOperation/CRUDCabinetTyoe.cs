e69ec1b34f5a962a8172829eaa517244ba94cc14
Daniil_Morozov <dannilmorozov@gmail.com>
2023-12-25 05:01:31 +0300

    Создать класс CRUDCabinetType и его реализацию

---
+++ CurriculumSchedule/CurriculumSchedule/Models/CRUDOperation/CRUDCabinetTyoe.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CurriculumSchedule.Models.CRUDOperation
+{
+    internal class CRUDCabinetTyoe
+    {
+        public ObservableCollection<CabinetType> ReadCabinetType()
+        {
+            using (ScheduleContext context = new())
+            {
+                var cabinet = new ObservableCollection<CabinetType>([.. context.CabinetTypes]);
+                return cabinet;
+            }
+        }
+
+        public bool CreateCabinetType(string cabinetName, string discription)
+        {
+            {
+                bool created = false;
+                try
+                {
+                    using (ScheduleContext context = new())
+                    {
+                        CabinetType newCabinetType = new()
+                        {
+                            CabinetName = cabinetName,
+                            Discription = discription,
+                        };
+                        context.CabinetTypes.Add(newCabinetType);
+                        context.SaveChanges();
+                        created = true;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    created = false;
+                }
+                return created;
+            }
+        }
+
+        public bool UpdateCabinetType(CabinetType newCabinetType)
+        {
+            bool updated = false;
+            using (ScheduleContext context = new())
+            {
+                try
+                {
+
+                    CabinetType? oldCabinetType = context.CabinetTypes.FirstOrDefault(id => id.Idcabinet == newCabinetType.Idcabinet);
+                    if (oldCabinetType != null)
+                    {
+                        oldCabinetType.CabinetName = newCabinetType.CabinetName;
+                        oldCabinetType.Discription = newCabinetType.Discription;
+                        context.SaveChanges();
+                        updated = true;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    updated = false;
+                }
+            }
+            return updated;
+        }
+
+        public bool DeleteCabinetType(CabinetType deleteCabinetType)
+        {
+            bool deleted = false;
+            using (ScheduleContext context = new())
+            {
+                try
+                {
+                    context.CabinetTypes.Remove(deleteCabinetType);
+                    context.SaveChanges();
+                    deleted = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    deleted = false;
+                }
+            }
+            return deleted;
+        }
+
+    }
+}
--- CurriculumSchedule/CurriculumSchedule/ViewModel/MainWindowViewModel.cs
+++ CurriculumSchedule/CurriculumSchedule/ViewModel/MainWindowViewModel.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CurriculumSchedule.ViewModel.Base;
 
 namespace CurriculumSchedule.ViewModel
 {
-    internal class MainWindowViewModel
+    internal class MainWindowViewModel : TitleViewModel
     {
+
     }
 }
