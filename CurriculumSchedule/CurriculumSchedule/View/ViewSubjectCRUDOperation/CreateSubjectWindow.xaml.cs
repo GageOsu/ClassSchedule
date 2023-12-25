64e3e6ef0420e4a5bd86fbd13dc54d2c14ce74b6
Daniil_Morozov <dannilmorozov@gmail.com>
2023-12-25 03:05:13 +0300

    Создать Окна CreateSubjectWindow UpdateSubjectWindow

---
+++ CurriculumSchedule/CurriculumSchedule/View/ViewSubjectCRUDOperation/CreateSubjectWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CurriculumSchedule.View.ViewSubjectCRUDOperation
+{
+    /// <summary>
+    /// Логика взаимодействия для CreateSubjectWindow.xaml
+    /// </summary>
+    public partial class CreateSubjectWindow : Window
+    {
+        public CreateSubjectWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
